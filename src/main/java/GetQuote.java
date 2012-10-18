000efe7e2b2a18f409771c92fe5157e366364f35
Alan Derrick <seattlecamper@gmail.com>
2012-10-17 19:01:03 -0700

    added buttons to GUI

--- src/main/java/GetQuote.java
+++ src/main/java/GetQuote.java
@@ -8,36 +8,58 @@ package main.java;
  */
 
 import javax.swing.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-class GetQuote extends JFrame {
 
-    // Constructor:
+public class GetQuote extends JFrame {
+
     public GetQuote() {
-        setTitle("Stock Quote v 1.0");
-        setSize(300,200); // default size is 0,0
-        setLocation(10,200); // default is 0,0 (top left corner)
+        initUI();
+    }
 
-        /**
-         * Window Listeners
-         */
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
+    public final void initUI() {
+
+        JPanel panel = new JPanel();
+        getContentPane().add(panel);
+
+        panel.setLayout(null);
+
+        JButton quitButton = new JButton("Quit");
+        quitButton.setBounds(50, 60, 80, 30);
+        quitButton.setToolTipText("Exit the application");
+        quitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
                 System.exit(0);
-            } //windowClosing
-        } );
+            }
+        });
+
+        JButton getButton = new JButton("Quote");
+        getButton.setBounds(150, 60, 80, 30);
+        getButton.setToolTipText("Pulls stock quote");
+        getButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                System.exit(0);
+            }
+        });
+
+
+
+        panel.add(quitButton);
+        panel.add(getButton);
+
+        setTitle("StockQuote v 1.0");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
-        JFrame f = new GetQuote();
-        f.setVisible(true);
-    } //main
-} //class GetQuote
-
-
-/* NOTES:
-	WindowAdapter() is class that implements WindowListeners
-	with null methods for all the 7 methods of WindowListeners!
-	It is found in java.awt.event.*.
- */
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                GetQuote ex = new GetQuote();
+                ex.setVisible(true);
+            }
+        });
+    }
+}
